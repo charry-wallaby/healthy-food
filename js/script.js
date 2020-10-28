bd237ec804ce4e29df8d83f67c084824879c5451
Maksim <89sagitarius@gmail.com>
2020-10-28 23:51:59 +0300

    поменял дату окончания таймера

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
  tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
  modal('[data-modal]', '.modal', modalTimerId);
- timer('.timer', '2020-08-11');
+ timer('.timer', '2020-12-11');
  cards();
  calc();
  forms('form', modalTimerId);
